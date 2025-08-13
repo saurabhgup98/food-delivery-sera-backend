2229379e64974cb370d0256d4fc6cd3a643f0e05
saurabhgup98 <saurabhgup98@gmail.com>
2025-08-13 19:20:33 +0530

    updated index

--- src/index.ts
+++ src/index.ts
@@ -37,23 +37,8 @@ app.use(helmet({
 }));
 
 // CORS configuration
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://food-delivery-sera.vercel.app',
-  process.env['CORS_ORIGIN']
-].filter(Boolean);
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: ['http://localhost:3000', 'https://food-delivery-sera.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
